eb4870be447c5888bf110d9ce1cf043f74105593
hashansl <hashan.dan@gmail.com>
2024-05-06 09:34:31 -0400

    updated

--- tennessee/CustomDataset_peristence_img.py
+++ tennessee/CustomDataset_peristence_img.py
@@ -18,34 +18,15 @@ class data_loader_persistence_img(Dataset):
         self.transform = transform
 
 
-
-        # self.variable_names = [name for name in os.listdir(root_dir) if not name.startswith('.')]
-
-
-        # dictionary to store image paths for each variable name
-        # self.image_paths = {variable_name: [] for variable_name in self.variable_names}
-        
-
-        # # Collect paths of all images in each variable name folder
-        # for variable_name in self.variable_names:
-        #     variable_folder = os.path.join(root_dir, variable_name)
-        #     # image_names = os.listdir(variable_folder)
-        #     image_names = [f for f in os.listdir(variable_folder) if not f.startswith('.')]
-        #     # image_names = [f for f in os.listdir(variable_folder) 
-        #             #    if os.path.isdir(os.path.join(variable_folder, f)) and not f.startswith('.')]
-        #     for image_name in image_names:
-        #         image_path = os.path.join(variable_folder, image_name)
-        #         self.image_paths[variable_name].append(image_path)
-
     def __len__(self):
-        # return sum(len(paths) for paths in self.image_paths.values())
-        pass
+        return len(self.annotations)
     
     def __getitem__(self,index):
 
-        img_path = os.path.join(self.root_dir, self.annotations.iloc[index, 0])
+        img_path = os.path.join(self.root_dir, self.annotations.iloc[index,4] + '.png')
+
         img = io.imread(img_path)
-        y_label = torch.tensor(int(self.annotations.iloc[index, 1]))
+        y_label = torch.tensor(int(self.annotations.iloc[index]['percentile']))
 
         if self.transform:
             img = self.transform(img)
@@ -53,30 +34,8 @@ class data_loader_persistence_img(Dataset):
         return (img, y_label)
         
 
-        # temp_dict = {}
-        pass
+custom_dataset = data_loader_persistence_img(geo_file_path='/Users/h6x/ORNL/git/opioid-risk-modeling/tennessee/data/processed data/SVI2018 TN counties with death rate HepVu/SVI2018_TN_counties_with_death_rate_HepVu.shp',
+    root_dir='/Users/h6x/ORNL/git/opioid-risk-modeling/tennessee/results/persistence images/percentiles/H0H1/EP_POV')
 
-        # for variable_name, paths in self.image_paths.items():
-        #     if index < 95:
-        #         image_path = paths[index]
-        #         image = io.imread(image_path)
-
-        #         if self.transform:
-        #             image = self.transform(image)
-
-        #         #store variable name and image in a dictionary
-        #         temp_dict[variable_name] = image
-
-        #     else:
-        #         raise IndexError('Index out of bounds')
-        
-        # return temp_dict
-
-
-
-# # Create custom dataset
-# custom_dataset = data_loader_persistence_img(geo_file_path='/Users/h6x/ORNL/git/opioid-risk-modeling/tennessee/data/processed data/SVI with HepVu census tracts/SVI2018 TN census tracts with death rate HepVu/SVI2018_TN_census_tracts_with_death_rate_HepVu.shp',
-#     root_dir='/Users/h6x/ORNL/git/opioid-risk-modeling/tennessee/results/persistence images/percentiles/H0H1/EP_POV')
-
-
-#single_observation = custom_dataset[94]
+# get the length of the dataset
+print(len(custom_dataset))
